944f604e4e4f8935574f614b9525ab094a730065
Ali Taghawi <marshaltaghawi@gmail.com>
2025-06-30 00:09:20 +0330

    creating user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: String,
+    lastName: String,
+    todos: {
+      type: [
+        {
+          todo: String,
+          status: {
+            type: String,
+            enum: "inprogress" | "done" | "review",
+            required: true,
+          },
+        },
+      ],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
