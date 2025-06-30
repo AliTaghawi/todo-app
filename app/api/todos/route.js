545b6d621649070f67e94be1cf1764459338db1d
Ali Taghawi <marshaltaghawi@gmail.com>
2025-06-30 15:03:06 +0330

    developing GET method in todos api

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import connectDB from "@/utils/connectDB";
 import User from "@/models/User";
 import { todoSchema } from "@/utils/validation";
+import { sortTodos } from "@/utils/helperFuncs";
 
 export async function POST(req) {
   try {
@@ -36,6 +37,36 @@ export async function POST(req) {
 
     user.todos.push({ title, description, status });
     user.save();
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal server error!" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req) {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "You are unauthorized!" },
+        { status: 403 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json({ error: "Can't find user!" }, { status: 404 });
+    }
+
+    const todos = sortTodos(user.todos);
+
+    return NextResponse.json({ todos }, { status: 200 });
+
   } catch (error) {
     console.log(error);
     return NextResponse.json(
---
+++ utils/helperFuncs.js
@@ -0,0 +1,10 @@
+const sortTodos = (todos) => {
+  const sortedTodos = {};
+  todos.map((todo) => {
+    if (!sortedTodos[todo.status]) sortedTodos[todo.status] = [];
+    sortedTodos[todo.status].push(todo);
+  });
+  return sortedTodos;
+};
+
+export { sortTodos };
