62cae57b46492ae5031308f53003027c1354e1d3
Ali Taghawi <marshaltaghawi@gmail.com>
2025-07-03 04:39:13 +0330

    developing change button fo updating password

--- components/modules/ChangePassword.js
+++ components/modules/ChangePassword.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import ProfileInput from "components/elements/ProfileInput";
 
-const buttonStyle = "bg-neutral-400 text-neutral-900 hover:bg-neutral-300 hover:outline-neutral-900 hover:outline-1 py-0.5 px-4 rounded-md mt-8";
-
+const buttonStyle =
+  "bg-neutral-400 text-neutral-900 hover:bg-neutral-300 hover:outline-neutral-900 hover:outline-1 py-0.5 px-4 rounded-md mt-8";
 
 const ChangePassword = ({ setUpdatePass }) => {
   const [data, setData] = useState({
@@ -15,7 +16,22 @@ const ChangePassword = ({ setUpdatePass }) => {
     const { name, value } = e.target;
     setData((prev) => ({ ...prev, [name]: value }));
   };
-  
+
+  const updateHandler = async () => {
+    const result = await fetch("/api/update-password", {
+      method: "PATCH",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    });
+    const res = await result.json();
+    if (res.error) {
+      toast.error(res.error);
+    } else {
+      toast.success(res.message);
+      setUpdatePass(false);
+    }
+  };
+
   return (
     <div>
       <ProfileInput
@@ -40,7 +56,9 @@ const ChangePassword = ({ setUpdatePass }) => {
         onChange={changeHandler}
       />
       <div className="flex justify-between items-center">
-        <button className={buttonStyle}>Change</button>
+        <button className={buttonStyle} onClick={updateHandler}>
+          Change
+        </button>
         <button className={buttonStyle} onClick={() => setUpdatePass(false)}>
           Cancel
         </button>
